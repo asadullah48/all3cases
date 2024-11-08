27c0a724afa92c27660f3c1bd61c42bd4f240f1c
Asadullah Shafique <asadullahshafique@hotmail.com>
2024-11-08 20:09:36 +0500

    revesion & edited

--- solution3.js
+++ solution3.js
@@ -1,4 +1,6 @@
-var name = "Ahmed";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "Ahmed";
 console.log(name.toLowerCase());
 console.log(name.toUpperCase());
 console.log(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
